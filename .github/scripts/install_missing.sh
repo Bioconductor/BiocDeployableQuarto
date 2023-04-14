679e7408e27f8cc278a889643d556a94a91adaf4
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2023-04-14 13:06:22 -0400

    Add RSPM

--- .github/scripts/install_missing.sh
+++ .github/scripts/install_missing.sh
@@ -8,10 +8,11 @@ while ( quarto render $QMD --to html 3>&1 1>&2- 2>&3- ) | grep "Error in loadNam
 do
     if [[ -s /tmp/rmissingpkg ]]; then
         RPKG=$(awk '{print $NF}' /tmp/rmissingpkg)
+        # Install missing R package
+        Rscript -e "if (!require('rspm')) install.packages('rspm'); rspm::enable(); BiocManager::install($RPKG)" && rm /tmp/rmissingpkg
         # Link reticulate with existing python3
         if [ $RPKG == "'reticulate'" ]; then Rscript -e "library(reticulate); use_python('$(which python3)')"; fi
-        # Install missing R package
-        Rscript -e "BiocManager::install($RPKG)" && rm /tmp/rmissingpkg
+        
     elif [[ -s /tmp/pymissingmodule ]]; then
         PYMOD=$(awk '{print $NF}' /tmp/pymissingmodule | sed "s/'//g")
         # Install missing python module
