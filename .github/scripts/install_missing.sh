a3d299910d8b367b98bac8d9acc05f19a9575348
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2023-04-22 01:17:05 -0400

    Update install_missing.sh

--- .github/scripts/install_missing.sh
+++ .github/scripts/install_missing.sh
@@ -11,7 +11,7 @@ do
     if [[ -s /tmp/rmissingpkg ]]; then
         RPKG=$(awk '{print $NF}' /tmp/rmissingpkg)
         # Install missing R package
-        Rscript -e "BiocManager::install('$(echo "${RPKG:1:${#RPKG}-2}")')" && rm /tmp/rmissingpkg
+        Rscript -e "BiocManager::install('$(echo "${RPKG:1:${#RPKG}-2}" | sed "s/'//g")')" && rm /tmp/rmissingpkg
         # Link reticulate with existing python3
         if [ $RPKG == "'reticulate'" ]; then Rscript -e "library(reticulate); use_python('$(which python3)')"; fi
         
